a35d88e53d21ff2511ff9228cfec04e1705e8e8b
komandante999 <dreftware@yandex.ru>
2024-02-04 20:17:37 +0700

    finished progekt

---
+++ gulpfile.js
@@ -0,0 +1,215 @@
+let preprocessor = 'sass', // Preprocessor (sass, less, styl); 'sass' also work with the Scss syntax in blocks/ folder.
+	fileswatch = 'html,htm,txt,json,md,woff2' // List of files extensions for watching & hard reload
+
+import pkg from 'gulp'
+const { src, dest, parallel, series, watch } = pkg
+
+import browserSync from 'browser-sync'
+import bssi from 'browsersync-ssi'
+import ssi from 'ssi'
+import webpackStream from 'webpack-stream'
+import webpack from 'webpack'
+import TerserPlugin from 'terser-webpack-plugin'
+import gulpSass from 'gulp-sass'
+import * as dartSass from 'sass'
+const sass = gulpSass(dartSass)
+import sassglob from 'gulp-sass-glob'
+import less from 'gulp-less'
+import lessglob from 'gulp-less-glob'
+import styl from 'gulp-stylus'
+import stylglob from 'gulp-noop'
+import postCss from 'gulp-postcss'
+import cssnano from 'cssnano'
+import autoprefixer from 'autoprefixer'
+import imagemin from 'gulp-imagemin'
+import avif from 'gulp-avif'
+import webp from 'gulp-webp'
+import compressImages from 'compress-images'
+import changed from 'gulp-changed'
+import concat from 'gulp-concat'
+import rsync from 'gulp-rsync'
+import { deleteAsync } from 'del'
+import ghPages from 'gulp-gh-pages';
+
+function browsersync() {
+	browserSync.init({
+		server: {
+			baseDir: 'app/',
+			middleware: bssi({ baseDir: 'app/', ext: '.html' })
+		},
+		ghostMode: { clicks: false },
+		notify: false,
+		online: true,
+		// tunnel: 'yousutename', // Attempt to use the URL https://yousutename.loca.lt
+	})
+}
+
+function scripts() {
+	return src(['app/js/*.js', '!app/js/*.min.js'])
+		.pipe(webpackStream({
+			mode: 'production',
+			performance: { hints: false },
+			plugins: [
+				new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery', 'window.jQuery': 'jquery' }), // jQuery (npm i jquery)
+			],
+			module: {
+				rules: [
+					{
+						test: /\.m?js$/,
+						exclude: /(node_modules)/,
+						use: {
+							loader: 'babel-loader',
+							options: {
+								presets: ['@babel/preset-env'],
+								plugins: ['babel-plugin-root-import']
+							}
+						}
+					}
+				]
+			},
+			optimization: {
+				minimize: true,
+				minimizer: [
+					new TerserPlugin({
+						terserOptions: { format: { comments: false } },
+						extractComments: false
+					})
+				]
+			},
+		}, webpack)).on('error', function handleError() {
+			this.emit('end')
+		})
+		.pipe(concat('app.min.js'))
+		.pipe(dest('app/js'))
+		.pipe(browserSync.stream())
+}
+
+function styles() {
+	return src([`app/styles/${preprocessor}/*.*`, `!app/styles/${preprocessor}/_*.*`])
+		.pipe(eval(`${preprocessor}glob`)())
+		.pipe(eval(preprocessor)({ 'include css': true }))
+		.pipe(postCss([
+			autoprefixer({ grid: 'autoplace' }),
+			cssnano({ preset: ['default', { discardComments: { removeAll: true } }] })
+		]))
+		.pipe(concat('app.min.css'))
+		.pipe(dest('app/css'))
+		.pipe(browserSync.stream())
+}
+
+function images() {
+	return src(['app/images/src/**/*'])
+		.pipe(changed('app/images/dist'))
+		.pipe(imagemin())
+		.pipe(dest('app/images/dist'))
+		.pipe(browserSync.stream())
+}
+
+async function imgCompress() {
+	compressImages(
+		`app/images/src/**/*.{jpg,JPG,jpeg,JPEG,png,PNG,gif,svg}`,
+		`app/images/dist/`,
+		{ compress_force: false, statistic: true, autoupdate: true },
+		false,
+		{ jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+		{ png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
+		{ svg: { engine: "svgo", command: "--multipass" } },
+		{ gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
+		function (err, completed) {
+			if (completed === true) {
+			}
+		}
+	);
+}
+
+function imgWebp() {
+	return src(`app/images/src/**/*.{jpg,JPG,jpeg,JPEG,png,PNG}`)
+		.pipe(webp({ quality: 40 }))
+		.pipe(dest(`app/images/dist/`))
+		.pipe(browserSync.stream())
+}
+
+function imgAvif() {
+	return src(`app/images/src/**/*.{jpg,JPG,jpeg,JPEG,png,PNG}`)
+		.pipe(avif({ quality: 50 }))
+		.pipe(dest(`app/images/dist/`))
+		.pipe(browserSync.stream())
+}
+
+
+function buildcopy() {
+	return src([
+		'{app/js,app/css}/*.min.*',
+		'app/images/**/*.*',
+		'!app/images/src/**/*',
+		'app/fonts/**/*'
+	], { base: 'app/' })
+		.pipe(dest('dist'))
+}
+
+async function buildhtml() {
+	let includes = new ssi('app/', 'dist/', '/**/*.html')
+	includes.compile()
+	await deleteAsync('dist/parts', { force: true })
+}
+
+async function cleandist() {
+	await deleteAsync('dist/**/*', { force: true })
+}
+
+function deploy() {
+	return src('dist/')
+		.pipe(rsync({
+			root: 'dist/',
+			hostname: 'username@yousite.com',
+			destination: 'yousite/public_html/',
+			clean: true, // Mirror copy with file deletion
+			// include: ['*.htaccess'], // Includes files to deploy
+			exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excludes files from deploy
+			recursive: true,
+			archive: true,
+			silent: false,
+			compress: true
+		}))
+}
+
+function deployGHPages() {
+	return src(`dist/`)
+		.pipe(ghPages())
+}
+exports.deployghp = deployGHPages
+
+function startwatch() {
+	watch([`app/styles/${preprocessor}/**/*`], { usePolling: true }, styles)
+	watch(['app/js/**/*.js', '!app/js/**/*.min.js'], { usePolling: true }, scripts)
+	watch(['app/images/src/**/*'], { usePolling: true }, images)
+	watch([`app/**/*.{${fileswatch}}`], { usePolling: true }).on('change', browserSync.reload)
+}
+
+export {
+	scripts,
+	styles,
+	images,
+	deploy
+}
+export let assets = series(scripts, styles, images)
+export let build = series(
+	cleandist,
+	images,
+	// imgCompress,
+	imgWebp,
+	imgAvif,
+	scripts,
+	styles,
+	buildcopy,
+	buildhtml
+)
+
+export default series(
+	scripts,
+	styles,
+	images,
+	// imgCompress,
+	imgWebp,
+	imgAvif,
+	parallel(browsersync, startwatch))
